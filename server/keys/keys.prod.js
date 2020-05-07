960042d5cb892c80625b914cd96151b8a48bd95d
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2020-05-07 16:52:52 +0600

    fix keys

--- server/keys/keys.prod.js
+++ server/keys/keys.prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     MONGO_URI: process.env.MONGO_URI,
     JWT: process.env.JWT,
-    s3AccessKeyId: process.env.JWT,
+    s3AccessKeyId: process.env.s3AccessKeyId,
     s3SecretAccessKey: process.env.s3SecretAccessKey,
 };
